c9857a7424ca370a0bbbac2e4503308231ad493b
M. Efdal <m.efdal66@hotmail.com>
2022-12-22 09:27:02 +0300

    Proje dosyası ekle.

---
+++ KartOyunu/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace KartOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Gold;
+            this.start.Font = new System.Drawing.Font("Comic Sans MS", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.start.Location = new System.Drawing.Point(265, 261);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(227, 92);
+            this.start.TabIndex = 0;
+            this.start.Text = "Yeni Oyun";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ KartOyunu/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KartOyunu
+{
+    public partial class Form2 : Form
+    {
+        Random random = new Random();
+
+        public static string[] suits = new string[] { "R", "G", "B", "O" };
+        public static string[] value = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        public List<string> deck;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            PlayCard();
+            Card51.Image = Image.FromFile("C:\\Users\\HalaycıKedi\\source\\repos\\KartOyunu\\KartOyunu\\Deck\\" + deck[0] + ".jpg");
+            Card52.Image = Image.FromFile("C:\\Users\\HalaycıKedi\\source\\repos\\KartOyunu\\KartOyunu\\Deck\\" + deck[1] + ".jpg");
+            Card53.Image = Image.FromFile("C:\\Users\\HalaycıKedi\\source\\repos\\KartOyunu\\KartOyunu\\Deck\\" + deck[2] + ".jpg");
+            Card54.Image = Image.FromFile("C:\\Users\\HalaycıKedi\\source\\repos\\KartOyunu\\KartOyunu\\Deck\\" + deck[3] + ".jpg");
+            target_card.Image = Image.FromFile("C:\\Users\\HalaycıKedi\\source\\repos\\KartOyunu\\KartOyunu\\Deck\\" + deck[20] + ".jpg");
+        }
+
+        private int selectRandomCard()
+        {
+            int randomCard;
+            randomCard = random.Next(0, deck.Count);
+            return randomCard;
+        }
+
+        //Oyun kartlarının oluşturmasını başlatır:
+        public void PlayCard()
+        {
+            deck = GenerateDeck();
+            Shuffle(deck);
+
+            //Bu kısım test amaçlıdır. GenerateDeck() ve Shuffle() çalışıyor mu diye kontrol edilir (Çıktı olarak verir):
+            foreach (string card in deck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+        
+        //suits ve value listelerini kullanarak bir deste kart oluşurulur:
+        public static List<string> GenerateDeck()
+        {
+            List<string> newDeck = new List<string>();
+            foreach (string s in suits)
+            {
+                foreach (string v in value)
+                {
+                    newDeck.Add(s + v);
+                }
+            }
+            return newDeck;
+        }
+
+        //GenerateDeck()'de oluşturulan desteyi karmak için:
+        void Shuffle<T>(List<T> list)
+        {
+            System.Random random = new System.Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                int k = random.Next(n);
+                n--;
+                T temp = list[k];
+                list[k] = list[n];
+                list[n] = temp;
+            }
+        }
+
+
+    }
+}
